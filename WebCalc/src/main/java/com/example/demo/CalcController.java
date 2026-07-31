e8548366aa1486c0937753496b765265d43bec2f
amit <av24soft@gmail.com>
2026-07-31 08:41:39 +0530

    1st commit

---
+++ WebCalc/src/main/java/com/example/demo/CalcController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+public class CalcController {
+
+	@GetMapping("calc/{op}/{a}/{b}")
+	int calculation(@PathVariable String op, @PathVariable int a, @PathVariable int b) {
+
+		if (op.equals("add") ) {
+			return a + b;
+		}
+
+		if (op.equals("sub")) {
+			return a - b;
+		}
+		if (op.equals("mul")) {
+			return a * b;
+		}
+
+		if (op.equals("div")) {
+
+			return a / b;
+		}
+
+		return 0;
+	}
+
+}
